144ad68279d4087671c782abadb338da34404b46
Mario Nebl <hello@herebecode.com>
2016-12-23 11:44:50 +0100

    chore: simplify build

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
+import {parse} from 'url';
+import caw from 'caw';
 import fetch from 'isomorphic-fetch';
-import createProxyAgent from './create-proxy-agent';
 
 const omniFetch = (url, options = {}) => {
 	// Respect explicitly passed agents
@@ -21,3 +22,25 @@ const omniFetch = (url, options = {}) => {
 // Be nice to typescript consumers
 omniFetch.default = omniFetch;
 export default omniFetch;
+
+/**
+ * Obtain a suitable proxy agent depending on environemt variables and url
+ * @param {string} url to get a suitable agent for
+ * @type {Object|null} agent suitable for application of current proxy settings,
+ * null if no proxy settings were given for the current protocol
+ */
+function createProxyAgent(url) {
+	// Determine environemt variable names to search for
+	const {protocol} = parse(url);
+	const protocolName = protocol.replace(':', '');
+	const envNames = [
+		`${protocolName}_proxy`,
+		`${protocolName}_proxy`.toUpperCase()
+	];
+
+	// Check if proxy is configured for current url protocol
+	const enabled = envNames.find(envName => process.env[envName]);
+
+	// Return suitable proxy agent, null of none configured
+	return enabled ? caw({protocol: protocolName}) : null;
+}
