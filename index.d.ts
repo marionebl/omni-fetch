cc58b62465dad401808354a37027b753b1d3ebfe
Markus Wolf <mail@markus-wolf.de>
2016-12-23 10:09:24 +0100

    chore: fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
-/// <reference path="typings/main.d.ts" />
-/// <reference path="isomorphic-fetch.d.ts" />
 declare module "omni-fetch" {
   import {RequestOptions} from "http";
-  import {RequestInit, Response} from "isomorphic-fetch";
+  import "isomorphic-fetch";
   export default function fetch(url: string, options?: RequestOptions | RequestInit): Promise<Response>;
 }
